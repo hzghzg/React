0be4ba6fd4a534042373684b6782ecc3ee61fe2b
hzghzg <2452376183@qq.com>
2020-08-04 22:59:36 +0800

    basic config

---
+++ src/index.js
@@ -0,0 +1,2 @@
+console.log('hello react')  
+//webpack-dev-server打包好的的main.js是放到内存中的，文件家中是看不到的
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path=require('path')
+const HtmlWebpackPlugin=require('html-webpack-plugin')
+const htmlPlugin=new HtmlWebpackPlugin({
+    template: path.join(__dirname,'./src/index.html'),
+    filename: 'index.html'
+})
+module.exports = {
+    mode: 'development',//有两种模式，development和production
+    //在webpack4.x中，需要额外装webpack-cli，且约定大于配置，默认的打包入口路径是src目录下的index
+    plugins: [htmlPlugin]
+}
