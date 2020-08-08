ffca8fc9eec82694a5bb58cccbebe8c3dcc2b527
hzghzg <2452376183@qq.com>
2020-08-08 11:51:21 +0800

    compare two implement of event

--- src/component/Hello.jsx
+++ src/component/Hello.jsx
@@ -16,10 +16,22 @@ class Hello extends React.Component {
         this.state.msg = '数据被改了'
         console.log(this.props.name)//用class创建的组件，可以不需要接受参数，可以直接通过this.props访问
         // return <h6 style={{color:'red'}}>This is a react component,{this.state.msg}</h6>
-        return <h6 className={hellocss.hclass} onClick={this.eventHandler}>This is a react component,{this.state.msg}</h6>
+        //方式一绑定事件：
+        // return <h6 className={hellocss.hclass} onClick={this.eventHandler}>This is a react component,{this.state.msg}</h6>
+        //方式二绑定事件：
+        return <h6 className={hellocss.hclass} onClick={()=>this.eventHandler()}>This is a react component,{this.state.msg}</h6>
     }
-    eventHandler() {
+    //方式一绑定事件：
+    // eventHandler() {
+    //     console.log(this)//这里的this是undefined的
+    //     console.log('be click')
+    //     this.state.msg='哈哈哈'
+    // }
+    //方式二绑定事件：这种方式是不会报错的
+    eventHandler=()=> {
+        console.log(this)
         console.log('be click')
+        this.state.msg='哈哈哈'
     }
 }
 
